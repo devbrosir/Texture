94c57783ab2f3df52f013e0f8cc271cf21917256
mohammad <khodadadi.mohammad2@gmail.com>
2026-07-27 22:17:26 +0330

    fix(User): remove uniqueness from create migration

--- modules/User/Database/migrations/0001_01_01_000000_create_users_table.php
+++ modules/User/Database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table): void {
             $table->id();
             $table->string('name')->nullable();
-            $table->string('email')->nullable()->unique();
+            $table->string('email')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('mobile')->nullable()->unique();
             $table->tinyInteger('role')->index();
