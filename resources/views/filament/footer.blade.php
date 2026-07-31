f40c55525d9f33ad5f263a300e32998ab291ac99
mohammad <khodadadi.mohammad2@gmail.com>
2026-07-31 17:26:28 +0330

    fix(footer): fix footer

--- resources/views/filament/footer.blade.php
+++ resources/views/filament/footer.blade.php
@@ -1,11 +1,10 @@
-<div class="p-2 text-center text-xs text-gray-500 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700">
+<div class="p-2 fixed bottom-0 left-0 right-0 bg-white text-center text-xs text-gray-500 dark:text-gray-400 border-t border-gray-200 dark:border-gray-700">
     <p class="flex items-center justify-center gap-2">
-        توسعه و طراحی توسط
-        <a href="https://zifrent.com/" class="font-medium text-primary-600 dark:text-primary-400 flex items-center gap-1">
+        طراحی و توسعه توسط
+        <a href="https://zifrent.com" target="_blank" class="font-medium text-primary-600 dark:text-primary-400 flex items-center gap-1">
             <img src="{{ asset('/images/zifrent-logo.png') }}"
                  alt="زیفرنت"
                  class="h-5 w-auto" />
-            زیفرنت
         </a>
     </p>
 </div>
